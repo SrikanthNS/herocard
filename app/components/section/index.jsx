2a20cbbce38497276afb3d12768ba989897ebaa0
Srikatnh N S <nssrikanth7@gmail.com>
2018-09-22 13:42:24 +0530

    Each Section will have state open and whole accordion will have state to know which section card is expanded

--- app/components/section/index.jsx
+++ app/components/section/index.jsx
@@ -1,60 +1,91 @@
-import React from 'react';
+import React, { Component } from 'react';
 import _ from 'lodash';
 import CardHolder from '../card-holder';
 import styles from './styles.scss';
 
+const imageMap = {
+  'BOOMI SFDC': require('../../images/Boomi@3x.png'),
+  SOCIALCAST: require('../../images/Socialcast@3x.png'),
+  SERVICENOW: require('../../images/ServiceNow@3x.png'),
+  SALESFORCE: require('../../images/Salesforce@3x.png'),
+  JIRA: require('../../images/Jira@3x.png'),
+  CONCUR: require('../../images/Concur@3x.png'),
+};
+
 /**
- * Section Pure component 
+ * Section Pure component
  * @param {object} - content
  * @param {function} - handle is a function passed from parent component is a onclock handelr
  * @param {number} - isCardExpanded is the index of the card if card is expanded else -1
  * @return {JSX} - will have card image, description and cardHolder component
  */
 
-export default function Section({ content, selectCard, isCardExpanded }) {
-  const imageMap = {
-    'BOOMI SFDC': require('../../images/Boomi@3x.png'),
-    SOCIALCAST: require('../../images/Socialcast@3x.png'),
-    SERVICENOW: require('../../images/ServiceNow@3x.png'),
-    SALESFORCE: require('../../images/Salesforce@3x.png'),
-    JIRA: require('../../images/Jira@3x.png'),
-    CONCUR: require('../../images/Concur@3x.png'),
-  };
+export default class Accordion extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { open: false };
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-  const imageSrc = (content.name && imageMap[_.toUpper(content.name)])
-    ? imageMap[_.toUpper(content.name)]
-    : require('../../images/Generic@3x.png');
+  componentWillReceiveProps(newProps) {
+    if (newProps.expandedCardIndex !== this.props.expandedCardIndex) {
+      if (newProps.expandedCardIndex === this.props.cardIndex) {
+        this.setState({ open: true });
+      } else {
+        this.setState({ open: false });
+      }
+    }
+  }
 
-  return (
-    <div
-      className={`hccf-hero-card ${isCardExpanded ? 'open' : ''} ${HeroCard.Utility.callbackClasses(content)}`}
-      onClick={selectCard}
-    >
-      <div className="hccf-card-header">
-        <div className="hccf-card-header__wrapper col-12 col-sm-12 col-md-12">
-          <div className="hccf-card-header__avatar">
-            <div>
-              <span>{imageSrc[content.name]}</span>
-              <img alt="avatar" src={imageSrc} />
+  handleClick(event, index) {
+    const { setExpandedCardIndex } = this.props;
+    if (_.includes(event.target.className, 'hccf-card-header__meta') ||
+      _.includes(event.target.className, 'hccf-hero-card open') ||
+      _.includes(event.target.className, 'hccf-hero-card')) {
+      setExpandedCardIndex(index);
+      this.setState({ open: !this.state.open });
+    }
+  }
+
+  render() {
+    const { open } = this.state;
+    const { content, cardIndex, expandedCardIndex } = this.props;
+    const imageSrc = (content.name && imageMap[_.toUpper(content.name)])
+      ? imageMap[_.toUpper(content.name)]
+      : require('../../images/Generic@3x.png');
+
+    return (
+      <div
+        className={`hccf-hero-card ${open && expandedCardIndex === cardIndex ?
+          'open' : ''} ${HeroCard.Utility.callbackClasses(content)}`}
+        onClick={e => this.handleClick(e, cardIndex)}
+      >
+        <div className="hccf-card-header">
+          <div className="hccf-card-header__wrapper col-12 col-sm-12 col-md-12">
+            <div className="hccf-card-header__avatar">
+              <div>
+                <span>{imageSrc[content.name]}</span>
+                <img alt="avatar" src={imageSrc} />
+              </div>
             </div>
-          </div>
-          <div className="hccf-card-header__meta">
-            <div className="hccf-card-header__meta-title">
-              {content.header.title}
-            </div>
-            <div className="hccf-card-header__meta-subtitle">
-              {content.header.subtitle}
+            <div className="hccf-card-header__meta">
+              <div className="hccf-card-header__meta-title">
+                {content.header.title}
+              </div>
+              <div className="hccf-card-header__meta-subtitle">
+                {content.header.subtitle}
+              </div>
             </div>
           </div>
         </div>
+        <div
+          key="content"
+          className={`hccf-row hccf-card-body ${open && expandedCardIndex === cardIndex ? 'open' : ''}`}
+        >
+          {open && expandedCardIndex === cardIndex ? <CardHolder cardContent={content} /> : null}
+        </div>
       </div>
-      <div
-        key="content"
-        className={`hccf-row hccf-card-body ${isCardExpanded ? 'open' : ''}`}
-      >
-        {isCardExpanded ? <CardHolder cardContent={content} /> : null}
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
