5d7aa0ff7e1ba78a3dde3b6ccc30cf099ade272d
Srikatnh N S <nssrikanth7@gmail.com>
2018-10-08 18:27:25 +0530

    Lint fixes

--- app/utility/event-emitter.js
+++ app/utility/event-emitter.js
@@ -12,54 +12,53 @@
 /**
  * EventEmitter
  * Function constructor for attaching and emitting custom events
- * 
+ *
  * @member events - Events list
  * @method on - Attach an event
  * @method emit - Handle an emitted event
  */
 const HeroCard = window.HeroCard || {};
 function EventEmitter() {
-    this.events = {};
-    
-    this.on = function (eventType, eventListener) {
-      this.events[eventType] = this.events[eventType] || [];
-      this.events[eventType].push(eventListener);
-    };
+  this.events = {};
 
-    this.emit = function (eventType, eventInfo) {
-      eventInfo = (typeof eventInfo !== undefined) ? eventInfo : {};
+  this.on = function (eventType, eventListener) {
+    this.events[eventType] = this.events[eventType] || [];
+    this.events[eventType].push(eventListener);
+  };
 
-      if (this.events[eventType]) {
-        this.events[eventType].forEach((eventListener) => {
-          eventListener(eventInfo);
-        });
-      }
-    };
+  this.emit = function (eventType, eventInfo) {
+    eventInfo = (typeof eventInfo !== undefined) ? eventInfo : {};
+
+    if (this.events[eventType]) {
+      this.events[eventType].forEach((eventListener) => {
+        eventListener(eventInfo);
+      });
+    }
+  };
 }
-  
+
 /**
  * HeroCardEventEmitter
  * Initialize and/or return EventEmitter
  */
 function HeroCardEventEmitter() {
-    function init() {
-        HeroCard.EventEmitter = HeroCard.EventEmitter || new EventEmitter();
-    }
-  
-    return {
-        initEventEmitter: init,
-        EventEmitter: function() {
-            if (HeroCard.EventEmitter) {
-                return HeroCard.EventEmitter;
-            } else {
-                console.error('EventEmitter is not initialized!');
-            }
-        }
-    };
+  function init() {
+    HeroCard.EventEmitter = HeroCard.EventEmitter || new EventEmitter();
+  }
+
+  return {
+    initEventEmitter: init,
+    EventEmitter() {
+      if (HeroCard.EventEmitter) {
+        return HeroCard.EventEmitter;
+      }
+      console.error('EventEmitter is not initialized!');
+    },
+  };
 }
 
 /**
  * Exports
  */
 export default HeroCardEventEmitter;
-  
+
